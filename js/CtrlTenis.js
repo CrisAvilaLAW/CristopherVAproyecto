a785d6010e7944d3ff6e287a18fe508c41ae32b2
CrisAvilaLAW <cristophervalier.cv@gmail.com>
2021-06-08 16:54:21 -0500

    ffffff

--- js/CtrlTenis.js
+++ js/CtrlTenis.js
@@ -35,7 +35,7 @@ async function protege(usuario) {
 
 function consulta() {
   daoTenis.
-    orderBy("marca").orderBy("modelo")
+    orderBy("marca")
     .onSnapshot(
       htmlLista, errConsulta);
 }
