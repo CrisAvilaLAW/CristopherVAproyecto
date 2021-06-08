ee9d81291c0df982728a5836071d1ffc0a286804
CrisAvilaLAW <84542551+CrisAvilaLAW@users.noreply.github.com>
2021-06-08 10:14:33 -0500

    Update CtrlUsuario.js

--- js/CtrlUsuario.js
+++ js/CtrlUsuario.js
@@ -18,7 +18,7 @@ import {
 import {
   checksRoles,
   guardaUsuario,
-  selectPasatiempos
+  selectTenis
 } from "./usuarios.js";
 
 const params =
@@ -57,9 +57,9 @@ async function busca() {
       forma.cue.value = id || "";
       img.src =
         await urlStorage(id);
-      selectPasatiempos(
-        forma.pasatiempoId,
-        data.pasatiempoId)
+      selectTenis(
+        forma.tenisId,
+        data.tenisId)
       checksRoles(
         listaRoles, data.rolIds);
       forma.addEventListener(
