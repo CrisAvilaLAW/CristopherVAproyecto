c0f9a43d136bb5ca0d5e2161ddb45a0dba45f437
CrisAvilaLAW <84542551+CrisAvilaLAW@users.noreply.github.com>
2021-06-09 19:46:30 -0500

    Update CtrlTenis.js

--- js/CtrlTenis.js
+++ js/CtrlTenis.js
@@ -1,10 +1,10 @@
 import {
   getAuth,
   getFirestore
-} from "../lib/fabricaa.js";
+} from "../lib/fabrica.js";
 import {
   urlStorage
-} from "../lib/storagee.js";
+} from "../lib/storage.js";
 import {
   cod,
   muestraError
